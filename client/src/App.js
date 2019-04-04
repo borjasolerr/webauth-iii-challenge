c30b896f2ed3348334beba2eedb413e80b71c31e
borjasolerr <borjasolrey@gmail.com>
2019-04-04 16:58:57 +0200

    Addednavbar, login, signup in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// Components
+import Register from './components/Register';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h3>Welcome to the home page</h3>
+
+      <Route path="/signup" component={Register} />
+
+      <Route path="/login" component={Login} />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/signup">Signup</NavLink>
+        <NavLink to="/login">Login</NavLink>
+      </nav>
+    </div>
+  );
+}
